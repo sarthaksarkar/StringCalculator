9aab943bc9ee604421ce92f6490e925c3bf459d3
Sarthak Sarkar <sarthaksarkar13@gmail.com>
2025-06-11 23:06:21 +0530

    new test added for NewLineAsSeparator

--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
+    StringCalculator calc=new StringCalculator();
+    @Test
+    public void NewlineAsSeparator(){
+        assertEquals("Number expected but '\n' found at position 6.",calc.add("175.2,\n35"));
+    }
 
 }
