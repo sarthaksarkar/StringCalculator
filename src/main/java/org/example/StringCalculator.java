ade2ab8a25966b0f53d870786cc9104fbd4a94c1
Sarthak Sarkar <sarthaksarkar13@gmail.com>
2025-06-11 23:39:29 +0530

    made tests for all the cases

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -1,5 +1,9 @@
 package org.example;
 
 public class StringCalculator {
+    public String add(String number){
 
+        return number;
+    }
 }
+
--- src/test/java/org/example/StringCalculatorTest.java
+++ src/test/java/org/example/StringCalculatorTest.java
@@ -10,5 +10,21 @@ class StringCalculatorTest {
     public void NewlineAsSeparator(){
         assertEquals("Number expected but '\n' found at position 6.",calc.add("175.2,\n35"));
     }
+    @Test
+    public void MissingNumberInLastPosition(){
+        assertEquals("Number expected but EOF found.",calc.add("1,3,"));
+    }
+    @Test
+    public void CustomSeparators(){
+        assertEquals("'|' expected but ',' found at position 3.",calc.add("//|\n1|2,3"));
+    }
+    @Test
+    public void NegativeNumbers(){
+        assertEquals("Negative not allowed : -4, -5",calc.add("2,-4,-5"));
+    }
+    @Test
+    public void MultipleErrors(){
+        assertEquals("Negative not allowed : -1\nNumber expected but ',' found at position 3.\nNegative not allowed : -2",calc.add("-1,,2"));
+    }
 
 }
