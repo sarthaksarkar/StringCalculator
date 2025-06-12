bc09a6c0162592fc6b0d38f4b3b924fabfb1a235
Sarthak Sarkar <sarthaksarkar13@gmail.com>
2025-06-12 08:05:54 +0530

    making a method to check whether the positioning of the separator and new line is correct

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -5,5 +5,16 @@ public class StringCalculator {
 
         return number;
     }
+    public boolean orientation(String delimeter,String number){
+        boolean correctOrientation=true;
+        if (number.substring(0,1).equals(delimeter) || number.substring(number.length()-1).equals(delimeter)){
+            correctOrientation=false;
+        } else{
+            for (int i=1;i<number.length()-1;i++){
+                
+            }
+        }
+        return correctOrientation;
+    }
 }
 
